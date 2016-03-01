d1d1a83bacdcd8a88e6f266941620a7b21cd7a95
axeno840 <axerosh@gmail.com>
2016-03-01 10:54:34 +0100

    Restructure of package, source and output

--- se.liu.ida.l2.tddd78.tetris/src/shipcomponents/AbstractShipComponent.java
+++ se.liu.ida.l2.tddd78.tetris/src/shipcomponents/AbstractShipComponent.java
@@ -1,22 +1,32 @@
 package shipcomponents;
 
-public abstract class AbstractShipComponent {
+/**
+ * An abstract implementation of the ShipComponent interface. Handles general ship component fucntionality including HP and
+ * HP loss.
+ */
+public abstract class AbstractShipComponent implements ShipComponent {
 
     /**
-     * The maximum integrity of this ship component. The damage it can take before it is destroyed.
+     * The maximum HP of this ship component. The damage it can take before it is destroyed.
      */
     private final int maxHp;
 
     /**
-     * The integrity left until destruction.
+     * The HP left until destruction.
      */
     private int hp;
 
-    public AbstractShipComponent(final int maxHp) {
+    /**
+     * Contrucs an abstract ship component with the specified maximum HP.
+     *
+     * @param maxHp the maximum HP if the ship component
+     */
+    protected AbstractShipComponent(final int maxHp) {
         this.maxHp = maxHp;
         hp = maxHp;
     }
 
+    @Override
     public void inflictDamage(int hp) {
         this.hp -= hp;
         this.hp = Math.max(hp, 0);
--- se.liu.ida.l2.tddd78.tetris/src/shipcomponents/ShipComponent.java
+++ se.liu.ida.l2.tddd78.tetris/src/shipcomponents/ShipComponent.java
@@ -1,17 +1,20 @@
 package shipcomponents;
 
+/**
+ * Interface defining the ship component and general functions including once for recieving damage and activation.
+ */
 public interface ShipComponent {
 
     /**
-     * Damages this ship component by reducing its integrity by the specified number of hit point.
+     * Damages this ship component by reducing its HP by the specified number of hit points.
      *
-     * @param hp the number of hit points by which this ship component's integrity is reduced
+     * @param hp the number of hit points by which this ship component's HP is reduced
      */
     public void inflictDamage(int hp);
 
     /**
      * Activates this ship component, performing its component-type specific action.
      */
-    public void activate();
+    public void performAction();
 
 }
