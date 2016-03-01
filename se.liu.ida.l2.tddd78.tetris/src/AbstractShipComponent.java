d3d8153b8959820ee3004429dcc41a0ebde22cdd
axeno840 <axerosh@gmail.com>
2016-03-01 10:41:46 +0100

    Restructure of package, source and output

---
+++ se.liu.ida.l2.tddd78.tetris/src/AbstractShipComponent.java
@@ -0,0 +1,22 @@
+public abstract class AbstractShipComponent {
+
+    /**
+     * The maximum integrity of this ship component. The damage it can take before it is destroyed.
+     */
+    private final int maxHp;
+
+    /**
+     * The integrity left until destruction.
+     */
+    private int hp;
+
+    public AbstractShipComponent(final int maxHp) {
+        this.maxHp = maxHp;
+        hp = maxHp;
+    }
+
+    public void inflictDamage(int hp) {
+        this.hp -= hp;
+        this.hp = Math.max(hp, 0);
+    }
+}
---
+++ se.liu.ida.l2.tddd78.tetris/src/ShipComponent.java
@@ -0,0 +1,15 @@
+public interface ShipComponent {
+
+    /**
+     * Damages this ship component by reducing its integrity by the specified number of hit point.
+     *
+     * @param hp the number of hit points by which this ship component's integrity is reduced
+     */
+    public void inflictDamage(int hp);
+
+    /**
+     * Activates this ship component, performing its component-type specific action.
+     */
+    public void activate();
+
+}
