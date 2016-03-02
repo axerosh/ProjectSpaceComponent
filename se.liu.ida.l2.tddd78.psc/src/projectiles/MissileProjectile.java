2e12a13d4b4fccd49778abf9eb2718399174a2e0
Axerosh <axerosh@gmail.com>
2016-03-02 08:28:00 +0100

    Extremely small change: Renamed "areaOfEffect" to "blastRaidus"

--- se.liu.ida.l2.tddd78.psc/src/projectiles/MissileProjectile.java
+++ se.liu.ida.l2.tddd78.psc/src/projectiles/MissileProjectile.java
@@ -5,8 +5,8 @@ import game.StarShip;
 public class MissileProjectile extends AbstractProjectile{
 
     public MissileProjectile(final float selfX, final float selfY, final float targetX, final float targetY,
-			     final float velocity, final StarShip enemyShip, final int damageOnImpact, final int areaOfEffect) {
-	super(selfX, selfY, targetX, targetY, velocity, enemyShip, damageOnImpact, areaOfEffect);
+			     final float velocity, final StarShip enemyShip, final int damageOnImpact, final int blastRadius) {
+		super(selfX, selfY, targetX, targetY, velocity, enemyShip, damageOnImpact, blastRadius);
     }
 
 
