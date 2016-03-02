d2fd374cfb88895b9d821b7052a84dbc21b4d50f
axeno840 <axerosh@gmail.com>
2016-03-02 16:49:30 +0100

    Test commit/push

--- se.liu.ida.l2.tddd78.psc/src/game/Test.java
+++ se.liu.ida.l2.tddd78.psc/src/game/Test.java
@@ -1,7 +1,6 @@
 package game;
 
 import graphics.GameComponent;
-import projectiles.MissileProjectile;
 import shipcomponents.ShipComponent;
 import shipcomponents.utilitycomponents.EngineComponent;
 import shipcomponents.utilitycomponents.ReactorComponent;
@@ -31,59 +30,59 @@ public final class Test {
 		playerShip.setComponent(engine, 2, 0);
 		playerShip.setComponent(engine, 3, 0);
 
-	playerShip.setComponent(missleComponent, 0, 1);
-	playerShip.setComponent(shield, 1, 1);
-	playerShip.setComponent(shield, 2, 1);
-	playerShip.setComponent(shield, 3, 1);
-	playerShip.setComponent(coolShield, 4, 1);
+		playerShip.setComponent(missleComponent, 0, 1);
+		playerShip.setComponent(shield, 1, 1);
+		playerShip.setComponent(shield, 2, 1);
+		playerShip.setComponent(shield, 3, 1);
+		playerShip.setComponent(coolShield, 4, 1);
 
-	playerShip.setComponent(engine, 0, 2);
-	playerShip.setComponent(engine, 2, 2);
-	playerShip.setComponent(engine, 4, 2);
+		playerShip.setComponent(engine, 0, 2);
+		playerShip.setComponent(engine, 2, 2);
+		playerShip.setComponent(engine, 4, 2);
 
-	playerShip.setComponent(engine, 0, 3);
-	playerShip.setComponent(engine, 1, 3);
-	playerShip.setComponent(engine, 2, 3);
-	playerShip.setComponent(engine, 3, 3);
-	playerShip.setComponent(engine, 4, 3);
+		playerShip.setComponent(engine, 0, 3);
+		playerShip.setComponent(engine, 1, 3);
+		playerShip.setComponent(engine, 2, 3);
+		playerShip.setComponent(engine, 3, 3);
+		playerShip.setComponent(engine, 4, 3);
 
-	playerShip.setComponent(reactor, 1, 4);
-	playerShip.setComponent(reactor, 3, 4);
+		playerShip.setComponent(reactor, 1, 4);
+		playerShip.setComponent(reactor, 3, 4);
 
-	field.addFriendlyShip(playerShip);
-	GameComponent gc = new GameComponent(field);
+		field.addFriendlyShip(playerShip);
+		GameComponent gc = new GameComponent(field);
 
-	JFrame frame = new JFrame("testing is FUUUUUUUUUUUUUUUUUUUUUUUN!");
-	frame.add(gc);
-	frame.pack();
-	frame.setVisible(true);
-	frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	gc.repaint();
+		JFrame frame = new JFrame("testing is FUUUUUUUUUUUUUUUUUUUUUUUN!");
+		frame.add(gc);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		gc.repaint();
 
-	playerShip.printShip();
+		playerShip.printShip();
 
-	for(int tick = 0; tick< 20; tick++){
-	    field.update();
-	    try{
-		Thread.sleep(1000);
-	    } catch(InterruptedException e) {
-		e.printStackTrace();
-	    }
+		for(int tick = 0; tick< 20; tick++){
+			field.update();
+			try{
+			Thread.sleep(1000);
+			} catch(InterruptedException e) {
+			e.printStackTrace();
+			}
 
-	    if(tick == 5){
-		missleComponent.giveOrder(new Order(15, 15, 8, 9, playerShip));
-		System.out.println("Order has been givin");
-	    }
-	    if(tick == 10){
-		missleComponent.increasePower();
-		System.out.println("Shoot should be fired");
-	    }
-	    gc.repaint();
+			if(tick == 5){
+			missleComponent.giveOrder(new Order(15, 15, 8, 9, playerShip));
+			System.out.println("Order has been givin");
+			}
+			if(tick == 10){
+			missleComponent.increasePower();
+			System.out.println("Shoot should be fired");
+			}
+			gc.repaint();
 
-	}
+		}
 
-	playerShip.printShip();
+		playerShip.printShip();
 
 
-    }
+		}
 }
